bf52a02024fce6f9572c7e5c1ee21db464883845
Dee Jay <42320330+dajoyce@users.noreply.github.com>
2019-02-12 19:02:22 -0500

    added Schema and Seeds

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,20 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS whiskey_db;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE DATABASE whiskey_db;
+
+USE whiskey_db;
+
+CREATE TABLE whiskeyTango (
+    id INT NOT NULL AUTO_INCREMENT,
+    whiskey_name VARCHAR(50) NULL,
+    rating INT NULL,
+    country VARCHAR(50) NULL,
+    category VARCHAR(50) NULL,
+    price INT NULL,
+    abv INT NULL,
+    age INT NULL,
+    brand INT NULL,
+    PRIMARY KEY (id)
+);
+
+SELECT * FROM whiskey_db
