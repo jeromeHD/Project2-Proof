cf222876c4d8bb218b041123859d3645646b2b6d
Ross Gongaware <ross@gongaware.org>
2019-02-14 17:21:49 -0500

    Converted schema to sequelize friendly js files in app/models

---
+++ app/models/whiskey.js
@@ -0,0 +1,44 @@
+module.exports = function (sequelize, Sequelize) {
+	var Whiskey = sequelize.define("whiskey", {
+		id: {
+			autoIncrement: true,
+			primaryKey: true,
+			type: Sequelize.INTEGER
+		},
+
+		whiskey_name: {
+			type: Sequelize.STRING,
+			notEmpty: true
+		},
+
+		rating: {
+			type: Sequelize.INTEGER,
+		},
+
+		country: {
+			type: Sequelize.STRING,
+		},
+
+		category: {
+			type: Sequelize.STRING,
+		},
+
+		price: {
+			type: Sequelize.INTEGER,
+		},
+
+		abv: {
+			type: Sequelize.INTEGER,
+		},
+
+		age: {
+			type: Sequelize.INTEGER,
+		},
+
+		brand: {
+			type: Sequelize.STRING
+		},
+	});
+
+	return Whiskey;
+};
