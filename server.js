916b2dc1710c7d3c9be02ee3cf660de937f3dbed
Ross Gongaware <ross@gongaware.org>
2019-02-14 17:46:18 -0500

    cleanup

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ var bodyParser = require("body-parser");
 var env = require("dotenv").load();
 var exphbs = require("express-handlebars");
 
+var PORT = process.env.PORT || 5000;
+
 //For BodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -21,15 +23,15 @@ app.use(passport.session()); // persistent login sessions
 //For Handlebars
 app.set("views", "./app/views");
 app.engine(
-  "hbs",
-  exphbs({
-    extname: ".hbs"
-  })
+	"hbs",
+	exphbs({
+		extname: ".hbs"
+	})
 );
 app.set("view engine", ".hbs");
 
-app.get("/", function(req, res) {
-  res.send("Welcome to Passport with Sequelize");
+app.get("/", function (req, res) {
+	res.send("Welcome to Passport with Sequelize");
 });
 
 //Models
@@ -43,15 +45,15 @@ require("./app/config/passport/passport.js")(passport, models.user);
 
 //Sync Database
 models.sequelize
-  .sync()
-  .then(function() {
-    console.log("Nice! Database looks fine");
-  })
-  .catch(function(err) {
-    console.log(err, "Something went wrong with the Database Update!");
-  });
+	.sync()
+	.then(function () {
+		console.log("Nice! Database looks fine");
+	})
+	.catch(function (err) {
+		console.log(err, "Something went wrong with the Database Update!");
+	});
 
-app.listen(5000, function(err) {
-  if (!err) console.log("Site is live");
-  else console.log(err);
+app.listen(PORT, function (err) {
+	if (!err) console.log("Site is live");
+	else console.log(err);
 });
