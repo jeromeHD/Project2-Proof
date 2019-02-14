0ca860a256c2ed8eb4dd8d4fc5c81caf8026b72e
Ross Gongaware <ross@gongaware.org>
2019-02-14 17:29:52 -0500

    fixed table names in sequelize models

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -1,5 +1,5 @@
 module.exports = function (sequelize, Sequelize) {
-	var Recipe = sequelize.define("whiskey", {
+	var Recipe = sequelize.define("recipe", {
 		id: {
 			autoIncrement: true,
 			primaryKey: true,
